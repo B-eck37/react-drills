e46500fcd605d2ae971871d9c62432bbc2b5eb21
B-eck37 <brent.eckert7@gmail.com>
2017-12-04 15:51:43 -0700

    Step/App 2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,7 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (){
+    super();
+    this.state = {
+      listOfThings: ["Tacos", "Salsa", "Tomatoes", "Lettuce", "Jalepenos"]
+    }
+  }
   render() {
+    let showFood = this.state.listOfThings.map(element =>
+      <h2>{element}</h2>
+    )
     return (
       <div className="App">
         <div className="App-header">
@@ -11,7 +20,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {showFood}
         </p>
       </div>
     );
