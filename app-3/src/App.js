e48631f69b635878644b447fefc8f82ed57200e3
B-eck37 <brent.eckert7@gmail.com>
2017-12-04 21:34:39 -0700

    app-3 - Had to use solution, confused on the need for index.

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (){
+    super();
+    this.state = {
+      listOfThings: ["tacos", "salsa", "tomatoes", "lettuce", "jalepenos"],
+      filteredList: "",
+    }
+  }
+  handleChange(value){
+    this.setState({ filteredList: value })
+  }
+
   render() {
+    let showList = this.state.listOfThings.filter((element,index) =>{
+      return element.includes(this.state.filteredList);
+    }).map((element,index) => {
+      return <h2>{element}</h2>
+    })
+    
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={ (event) => this.handleChange(event.target.value)} type="text" />
+          {showList}
       </div>
     );
   }
 }
 
 export default App;
+
